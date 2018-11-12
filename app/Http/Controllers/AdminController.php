0d57e8543456b7b8280929e0da6b162c74891a82
Michal Kuta <michal@webmedia.ie>
2018-11-12 12:45:29 +0000

    Chart.js added to admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\User;
+use App\Category;
+use App\Comment;
+use App\CommentReply;
 
 class AdminController extends Controller
 {
@@ -10,6 +15,13 @@ class AdminController extends Controller
 
     public function index()
     {
-    	return view('admin.index');
+    	$postCount = Post::count();
+    	$categoriesCount = Category::count();
+    	$commentsCount = Comment::count();
+    	$repliesCount = CommentReply::count();
+    	$usersCount = User::count();
+
+
+    	return view('admin.index', compact('postCount', 'categoriesCount', 'commentsCount', 'repliesCount', 'usersCount'));
     }
 }
